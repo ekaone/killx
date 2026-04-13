bb0c05cd8b9c48c1198fbc90f85df8f3fa186303
Eka Prasetia <ekaone3033@gmail.com>
2026-04-13 10:30:22 +0700

    chore: add JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * @file index.ts
+ * @description Core entry point for @ekaone/killx.
+ * @author Eka Prasetia
+ * @website https://prasetia.me
+ * @license MIT
+ */
+
 export { killx } from "./kill.js";
 export { inspect, buildTree, flattenTree, isAlive } from "./inspect.js";
 export type {
